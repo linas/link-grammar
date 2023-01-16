4d9838462ea4524b02bd8218ca2a8869752aeb75
Linas Vepstas <linasvepstas@gmail.com>
2023-01-16 14:43:33 -0600

    finish speller unit test

--- link-grammar/tokenize/aspell-memleak.c
+++ link-grammar/tokenize/aspell-memleak.c
@@ -14,98 +14,32 @@ int main()
 	AspellCanHaveError *spell_err = NULL;
 
 	config = new_aspell_config();
-	aspell_config_replace(config, ASPELL_LANG_KEY, "en_US");
+	aspell_config_replace(config, "lang", "en_US");
 
-	spell_err = new_aspell_speller(aspell->config);
+	spell_err = new_aspell_speller(config);
 	speller = to_aspell_speller(spell_err);
-}
 
-/**
- * Free memory structures used with spell-checker 'chk'
- */
-void spellcheck_destroy(void * chk)
-{
-	struct linkgrammar_aspell *aspell = (struct linkgrammar_aspell *)chk;
-	if (aspell)
-	{
-		delete_aspell_speller(aspell->speller);
-		delete_aspell_config(aspell->config);
-		free(aspell);
-		aspell = NULL;
-	}
-}
-
-/**
- * Ask the spell-checker if the spelling looks good.
- * Return true if the spelling is good, else false.
- */
-bool spellcheck_test(void * chk, const char * word)
-{
-	int val = 0;
-	struct linkgrammar_aspell *aspell = (struct linkgrammar_aspell *)chk;
-	if (aspell && aspell->speller)
+	char* word = "asdfasdfsadf";
+	for (int l=0; l<1000000; l++)
 	{
 		/* this can return -1 on failure */
-		val = aspell_speller_check(aspell->speller, word, -1);
-	}
-	return (val == 1);
-}
-
-// Despite having a thread-compatible API, it appears that apsell
-// is not actually thread-safe. Bummer.
-#if HAVE_PTHREAD
-static pthread_mutex_t aspell_lock = PTHREAD_MUTEX_INITIALIZER;
-#endif /* HAVE_PTHREAD */
-
-int spellcheck_suggest(void * chk, char ***sug, const char * word)
-{
-	struct linkgrammar_aspell *aspell = (struct linkgrammar_aspell *)chk;
-	if (!sug)
-	{
-		prt_error("Error: spellcheck_suggest: Corrupt pointer.\n");
-		return 0;
-	}
-
-	if (aspell && aspell->speller)
-	{
+		int badword = aspell_speller_check(speller, word, -1);
+printf("duude badword=%d\n", badword);
 		const AspellWordList *list = NULL;
 		AspellStringEnumeration *elem = NULL;
 		const char *aword = NULL;
-		unsigned int size, i;
-		char **array = NULL;
-
-		pthread_mutex_lock(&aspell_lock);
-		list = aspell_speller_suggest(aspell->speller, word, -1);
+		unsigned int size;
+		list = aspell_speller_suggest(speller, word, -1);
 		elem = aspell_word_list_elements(list);
 		size = aspell_word_list_size(list);
 
-		/* allocate an array of char* for returning back to link-parser */
-		array = (char **)malloc(sizeof(char *) * size);
-		if (!array)
-		{
-			prt_error("Error: spellcheck_suggest: Out of memory.\n");
-			delete_aspell_string_enumeration(elem);
-			pthread_mutex_unlock(&aspell_lock);
-			return 0;
-		}
-
-		i = 0;
 		while ((aword = aspell_string_enumeration_next(elem)) != NULL)
 		{
-			array[i++] = strdup(aword);
+			printf("got %s\n", aword);
 		}
 		delete_aspell_string_enumeration(elem);
-		*sug = array;
-		pthread_mutex_unlock(&aspell_lock);
-		return size;
 	}
-	return 0;
-}
 
-void spellcheck_free_suggest(void *chk, char **sug, int size)
-{
-	for (int i = 0; i < size; ++i) free(sug[i]);
-	free(sug);
+	delete_aspell_speller(speller);
+	delete_aspell_config(config);
 }
-
-#endif /* #ifdef HAVE_ASPELL */
