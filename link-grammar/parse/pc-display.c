1ce39aa02a26ec6e62c47496fdd021c6d93bbb69
Linas Vepstas <linasvepstas@gmail.com>
2023-03-08 13:34:09 -0600

    more draw

--- link-grammar/parse/pc-display.c
+++ link-grammar/parse/pc-display.c
@@ -2,25 +2,37 @@
 // See wg-display.c
 // dot -Txlib file
 
-static void draw_pchoice(dyn_str *pcd, Parse_choice * pc)
+static void pchoice_node(dyn_str *pcd, Parse_choice * pc)
 {
+	char buf[80];
+	sprintf(buf, "\"%s %d %d\"", pc->md->word_string, pc->l_id, pc->r_id);
+	dyn_strcat(pcd, buf);
 }
 
-static int pset_id = 0;
+static void draw_pchoice(dyn_str *pcd, Parse_choice * pc)
+{
+	pchoice_node(pcd, pc);
+	dyn_strcat(pcd, ";\n");
+
+	// if (pc->set[0])
+}
 
 static void draw_pset(dyn_str *pcd, Parse_set * pset)
 {
-	dyn_strcat(pcd, "foo->bar;\n");
+	if (!pset) return;
+
 	Parse_choice * pc = pset->first;
 	while (pc)
 	{
-		pset_id ++;
-		char buf[80];
-		sprintf(buf, "\"%s %d\"", pc->md->word_string, pset_id);
-		dyn_strcat(pcd, buf);
-		dyn_strcat(pcd, ";\n");
-
 		draw_pchoice(pcd, pc);
+
+		if (pc->next)
+		{
+			pchoice_node(pcd, pc);
+			dyn_strcat(pcd, " -> ");
+			pchoice_node(pcd, pc->next);
+			dyn_strcat(pcd, ";\n");
+		}
 		pc = pc->next;
 	}
 }
@@ -29,6 +41,7 @@ void display_parse_choice(extractor_t * pex)
 {
 	dyn_str *pcd = dyn_str_new();
 	dyn_strcat(pcd, "digraph {\n");
+	dyn_strcat(pcd, "rankdir=\"LR\";\n");
 
 	draw_pset(pcd, pex->parse_set);
 	dyn_strcat(pcd, "}\n");
