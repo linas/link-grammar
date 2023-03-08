efb15b165e564d6bd9639cdd272e32fa09252789
Linas Vepstas <linasvepstas@gmail.com>
2023-03-08 15:42:51 -0600

    More reorg

--- link-grammar/parse/pc-display.c
+++ link-grammar/parse/pc-display.c
@@ -17,77 +17,30 @@ static void pchoice_node(dyn_str *pcd, Parse_choice * pc)
 
 int nid = 0;
 
-// Append the main pset name
+// Append the main pset name, or null if null.
 static void draw_pset_name(dyn_str *pcd, Parse_set * pset)
-{
-	if (NULL == pset) return;  // Can't ever happen
-	if (NULL == pset->first)
-	{
-		char buf[80];
-		sprintf(buf, "\"%d\" ", nid);
-		dyn_strcat(pcd, buf);
-		return;
-	}
-
-	pchoice_node(pcd, pset->first);
-}
-
-static void draw_pset(dyn_str *, Parse_set *);
-static void draw_pset_recursive(dyn_str *, Parse_set *);
-
-static void draw_pchoice(dyn_str *pcd, Parse_choice * pc)
-{
-	dyn_strcat(pcd, "    { rank=same ");
-	draw_pset_name(pcd, pc->set[0]);
-	draw_pset_name(pcd, pc->set[1]);
-	dyn_strcat(pcd, " };\n");
-
-	dyn_strcat(pcd, "    ");
-	pchoice_node(pcd, pc);
-
-	dyn_strcat(pcd, " -> { ");
-	draw_pset_name(pcd, pc->set[0]);
-	draw_pset_name(pcd, pc->set[1]);
-	dyn_strcat(pcd, " };\n");
-
-	nid++;
-
-	// draw_pset(pcd, pc->set[0]);
-	// draw_pset(pcd, pc->set[1]);
-
-	draw_pset_recursive(pcd, pc->set[0]);
-	draw_pset_recursive(pcd, pc->set[1]);
-}
-
-static void draw_pset(dyn_str *pcd, Parse_set * pset)
 {
 	if (NULL == pset) return;  // Can't ever happen
 	if (NULL == pset->first)
 	{
 		nid ++;
 		char buf[80];
-		sprintf(buf, "\"%d\"", nid);
-		dyn_strcat(pcd, buf);
+		sprintf(buf, "\"%d\" ", nid);
+		// dyn_strcat(pcd, buf);
 		return;
 	}
 
+	pchoice_node(pcd, pset->first);
+}
+
+// Draw horizontal set of parse choices
+static void draw_pset_horizontal(dyn_str *pcd, Parse_set * pset)
+{
 	// Horizontal row for parse choices
-	dyn_strcat(pcd, "    subgraph HZ {\n");
+	dyn_strcat(pcd, "    subgraph HZ { rank=same \n");
 
-	// First tell system these are all on same row
-	Parse_choice * pc = pset->first;
-	dyn_strcat(pcd, "        { rank=same ");
-	while (pc)
-	{
-		pchoice_node(pcd, pc);
-		dyn_strcat(pcd, " ");
-		pc = pc->next;
-	}
-	dyn_strcat(pcd, "}\n");
-
-	// Now draw the horizontal list
 	dyn_strcat(pcd, "        ");
-	pc = pset->first;
+	Parse_choice * pc = pset->first;
 	while (pc)
 	{
 		pchoice_node(pcd, pc);
@@ -99,22 +52,40 @@ static void draw_pset(dyn_str *pcd, Parse_set * pset)
 	dyn_strcat(pcd, ";\n    };\n");
 }
 
+static void draw_pset_recursive(dyn_str *, Parse_set *);
+
+static void draw_pchoice(dyn_str *pcd, Parse_choice * pc)
+{
+	// Draw the left and right sides of binary tree.
+	dyn_strcat(pcd, "    ");
+	pchoice_node(pcd, pc);
+
+	dyn_strcat(pcd, " -> { rank=same ");
+	draw_pset_name(pcd, pc->set[0]);
+	draw_pset_name(pcd, pc->set[1]);
+	dyn_strcat(pcd, " };\n");
+
+	// draw_pset(pcd, pc->set[0]);
+	// draw_pset(pcd, pc->set[1]);
+
+	draw_pset_recursive(pcd, pc->set[0]);
+	draw_pset_recursive(pcd, pc->set[1]);
+}
+
 static void draw_pset_recursive(dyn_str *pcd, Parse_set * pset)
 {
 	if (NULL == pset->first) return;
 
-	// Now draw the vertical tree
-	dyn_strcat(pcd, "subgraph {\n");
-	draw_pset(pcd, pset);
+	// Draw the horizontal part first.
+	draw_pset_horizontal(pcd, pset);
 
-	// dyn_strcat(pcd, "rankdir=\"LR\";\n");
+	// Draw the binary pairs.
 	Parse_choice * pc = pset->first;
 	while (pc)
 	{
 		draw_pchoice(pcd, pc);
 		pc = pc->next;
 	}
-	dyn_strcat(pcd, "}\n");
 }
 
 void display_parse_choice(extractor_t * pex)
