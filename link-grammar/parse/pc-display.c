279e040453bb61c6e527063aff8deb97ef3705b5
Linas Vepstas <linasvepstas@gmail.com>
2023-03-08 13:10:56 -0600

    Start drawing

--- link-grammar/parse/pc-display.c
+++ link-grammar/parse/pc-display.c
@@ -2,15 +2,33 @@
 // See wg-display.c
 // dot -Txlib file
 
-void draw_pset(dyn_str *pcd, Parse_set * pset)
+static void draw_pchoice(dyn_str *pcd, Parse_choice * pc)
+{
+}
+
+static int pset_id = 0;
+
+static void draw_pset(dyn_str *pcd, Parse_set * pset)
 {
 	dyn_strcat(pcd, "foo->bar;\n");
+	Parse_choice * pc = pset->first;
+	while (pc)
+	{
+		pset_id ++;
+		char buf[80];
+		sprintf(buf, "\"%s %d\"", pc->md->word_string, pset_id);
+		dyn_strcat(pcd, buf);
+		dyn_strcat(pcd, ";\n");
+
+		draw_pchoice(pcd, pc);
+		pc = pc->next;
+	}
 }
 
 void display_parse_choice(extractor_t * pex)
 {
 	dyn_str *pcd = dyn_str_new();
-	dyn_strcat(pcd, "graph {\n");
+	dyn_strcat(pcd, "digraph {\n");
 
 	draw_pset(pcd, pex->parse_set);
 	dyn_strcat(pcd, "}\n");
